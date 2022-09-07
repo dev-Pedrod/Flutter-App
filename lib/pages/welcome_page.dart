06d295260f57933283c3b65f94d242ea2ab8d7e4
João Pedro <pedroolivxira@gmail.com>
2022-09-07 10:32:51 -0300

    feat(Pages): Welcome Page

---
+++ lib/pages/welcome_page.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class WelcomePage extends StatefulWidget {
+  const WelcomePage({super.key});
+
+  @override
+  State<WelcomePage> createState() => _WelcomePageState();
+}
+
+class _WelcomePageState extends State<WelcomePage> {
+  List images = [
+    "welcome-one.png",
+    "welcome-two.png",
+    "welcome-three.png",
+  ];
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView.builder(
+        scrollDirection: Axis.vertical,
+        itemCount: images.length,
+        itemBuilder: (_, index){
+          return Container(
+            width: double.maxFinite,
+            height: double.maxFinite,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage(
+                  "img/"+images[index]
+                )
+              )
+            ),
+          )
+        }),
+    );
+  }
+}
