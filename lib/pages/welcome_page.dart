ac63b9acd02b4bfaace9cd6ea92d59dcabd51c14
João Pedro <pedroolivxira@gmail.com>
2022-09-07 11:40:45 -0300

    feat(Pages): Add texts widgets to welcome page

--- lib/pages/welcome_page.dart
+++ lib/pages/welcome_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_app/misc/colors.dart';
+import 'package:flutter_app/widgets/app_large_text.dart';
+import 'package:flutter_app/widgets/app_text.dart';
 
 class WelcomePage extends StatefulWidget {
   const WelcomePage({super.key});
@@ -29,11 +30,35 @@ class _WelcomePageState extends State<WelcomePage> {
             decoration: BoxDecoration(
               image: DecorationImage(
                 image: AssetImage(
-                  "img/"+images[index]
-                )
-              )
+                  "img/${images[index]}"
+                ),
+                fit: BoxFit.cover
+              ),
             ),
-          )
+            child: Container(
+              margin: const EdgeInsets.only(top: 150, left: 20, right: 20),
+              child: Row(
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      AppLargeText(text: "Trips"),
+                      AppText(text: "Mountain", size: 30,),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      SizedBox(
+                        width: 250,
+                        child: AppText(
+                          text: "Mountain hikes give you an incredible sense of freedom along width endurance tests",
+                          color: AppColors.textColor2,
+                          size: 14,
+                          ),
+                      )
+                    ],
+                  )]),
+            ),
+          );
         }),
     );
   }
