09eab156e9e7f2b9603ae188522287391c49e2f6
João Pedro <pedroolivxira@gmail.com>
2022-09-08 08:30:32 -0300

    feat(Pages): Menu and tab bar at home page

--- lib/pages/navpages/home_page.dart
+++ lib/pages/navpages/home_page.dart
@@ -1,5 +1,9 @@
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+//libs
+import 'package:flutter/material.dart';
+// colors
+import 'package:flutter_app/misc/colors.dart';
+// widgets
+import 'package:flutter_app/widgets/app_large_text.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,9 +12,116 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    TabController tabController = TabController(length: 3, vsync: this);
+    return Scaffold(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            padding: const EdgeInsets.only(top: 70, left: 20),
+            child: Row(
+              children: [
+                const Icon(
+                  Icons.menu,
+                  size: 30,
+                  color: Colors.black54,
+                ),
+                Expanded(child: Container()),
+                Container(
+                  margin: const EdgeInsets.only(right: 20),
+                  width: 50,
+                  height: 50,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.grey.withOpacity(0.5),
+                  ),
+                )
+              ],
+            ),
+          ),
+          const SizedBox(
+            height: 40,
+          ),
+          Container(
+            margin: const EdgeInsets.only(left: 20),
+            child: AppLargeText(text: "Discover"),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          SizedBox(
+            child: Align(
+              alignment: Alignment.centerLeft,
+              child: TabBar(
+                labelPadding: const EdgeInsets.only(left: 20, right: 20),
+                controller: tabController,
+                labelColor: Colors.black,
+                unselectedLabelColor: Colors.grey,
+                isScrollable: true,
+                indicatorSize: TabBarIndicatorSize.label,
+                indicator: CircleTabIndicator(
+                  color: AppColors.mainColor,
+                  radius: 4),
+                tabs: const [
+                  Tab(text: "Places",),
+                  Tab(text: "Inspiration",),
+                  Tab(text: "Emotions",),
+                ],
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 300,
+            width: double.maxFinite,
+            child: TabBarView(
+              controller: tabController,
+              children: const [
+                Text("hi"),
+                Text("there"),
+                Text("bye"),
+              ]),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class CircleTabIndicator extends Decoration {
+  final Color color;
+  double radius;
+
+  CircleTabIndicator({
+    required this.color,
+    required this.radius
+  });
+
+  @override
+  BoxPainter createBoxPainter([VoidCallback? onChanged]) {
+    return CirclePainter(color: color, radius: radius);
+  }
+}
+class CirclePainter extends BoxPainter {
+  final Color color;
+  double radius;
+
+  CirclePainter({
+    required this.color,
+    required this.radius
+  });
+
+  @override
+  void paint(Canvas canvas, Offset offset, ImageConfiguration configuration) {
+    Paint paint = Paint();
+    paint.color = color;
+    paint.isAntiAlias =  true;
+    final Offset circleOffset = Offset(
+      configuration.size!.width/2 -radius/2,
+      configuration.size!.height-radius);
+
+    canvas.drawCircle(offset + circleOffset, radius, paint);
   }
 }
