6947d77f71d158c24ef36090f65c137d4afeddc3
João Pedro <pedroolivxira@gmail.com>
2022-09-12 08:21:40 -0300

    feat(pages): Update the data dynamically on detail page

--- lib/cubit/app_cubits_logics.dart
+++ lib/cubit/app_cubits_logics.dart
@@ -1,10 +1,15 @@
+// libs
 import 'package:flutter/material.dart';
+
+// cubits
 import 'package:flutter_app/cubit/app_cubit_states.dart';
 import 'package:flutter_app/cubit/app_cubits.dart';
-import 'package:flutter_app/pages/details_pages.dart';
-import 'package:flutter_app/pages/home_page.dart';
-import 'package:flutter_app/pages/welcome_page.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+
+// pages
+import '../pages/navpages/main_pages.dart';
+import 'package:flutter_app/pages/details_pages.dart';
+import 'package:flutter_app/pages/welcome_page.dart';
 
 class AppCubitLogics extends StatefulWidget {
   const AppCubitLogics({Key? key}) : super(key: key);
@@ -23,7 +28,7 @@ class _AppCubitLogicsState extends State<AppCubitLogics> {
             return const WelcomePage();
           }
           if (state is LoadedState) {
-            return const HomePage();
+            return const MainPage();
           }
           if (state is DetailsState) {
             return const DetailPage();
