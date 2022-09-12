474707411aaff86bd5f494619c63288962194d37
João Pedro <pedroolivxira@gmail.com>
2022-09-12 07:03:06 -0300

    feat(Cubits): Loading state and Loaded state

--- lib/cubit/app_cubits.dart
+++ lib/cubit/app_cubits.dart
@@ -1,8 +1,21 @@
 import 'package:bloc/bloc.dart';
 import 'package:flutter_app/cubit/app_cubit_states.dart';
 
+import '../services/data_service.dart';
+
 class AppCubits extends Cubit<CubitStates> {
-  AppCubits() : super(InitialState()) {
+  final DataService data;
+  late final places;
+
+  AppCubits({required this.data}) : super(InitialState()) {
     emit(WelcomeState());
+  }
+
+  void getData() async {
+    try {
+      emit(LoadingState());
+      places = await data.getInfo();
+      emit(LoadedState(places));
+    } catch (e) {}
   }
 }
